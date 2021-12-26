cfa2a610a099c9260ad4d7caeb2108e5741c0feb
TChriste <thibaudchriste1997@gmail.com>
2021-12-26 20:00:33 +0100

    Avatar

--- src/app/user/wallet/wallet.component.ts
+++ src/app/user/wallet/wallet.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import CredentialsInterface from 'src/app/interfaces/CredentialsInterface';
 import { UserService } from 'src/app/services/user.service';
+import { avatar } from 'src/avatar';
 
 @Component({
   selector: 'app-wallet',
@@ -11,15 +12,26 @@ export class WalletComponent implements OnInit {
 
   credientals: CredentialsInterface[] = [];
 
-
   constructor(
     private _userService: UserService
   ) { }
 
+  avatarRandom(type: string) {
+    return avatar[type][Math.floor(Math.random() * avatar[type].length)];
+  }
+
   ngOnInit(): void {
     this._userService.getCredentials().subscribe((credientals) => {
-      this.credientals = credientals.results
+      this.credientals = credientals.results;
     })
+  }
+
+  getAvatar() {
+    return 'https://avataaars.io/?avatarStyle=Circle&topType=' + this.avatarRandom('topType') +
+      '&accessoriesType=' + this.avatarRandom('accessoriesType') + '&hairColor=' + this.avatarRandom('hairColor') +
+      '&facialHairType=' + this.avatarRandom('facialHairType') + '&clotheType=' + this.avatarRandom('clotheType') +
+      '&eyeType=' + this.avatarRandom('eyeType') + '&eyebrowType=' + this.avatarRandom('eyebrowType') +
+      '&mouthType=' + this.avatarRandom('mouthType') + '&skinColor=' + this.avatarRandom('skinColor');
   }
 
 }
---
+++ src/avatar.ts
@@ -0,0 +1,102 @@
+export const avatar: any = {
+    topType : [
+      'NoHair',
+      'Eyepatch',
+      'Hat',
+      'Hijab',
+      'LongHairBigHair',
+      'LongHairBob',
+      'LongHairBun',
+      'LongHairCurly',
+      'LongHairCurvy',
+      'LongHairDreads',
+      'LongHairFro',
+      'LongHairFroBand',
+      'LongHairNotTooLong',
+      'LongHairShavedSides',
+      'LongHairMiaWallace',
+      'LongHairStraight',
+      'LongHairStraight2',
+      'LongHairStraightStrand',
+      'ShortHairDreads01',
+      'ShortHairDreads02',
+      'ShortHairFrizzle',
+      'ShortHairShaggyMullet',
+      'ShortHairShortCurly',
+      'ShortHairShortFlat',
+      'ShortHairShortRound',
+      'ShortHairShortWaved',
+      'ShortHairSides',
+      'ShortHairTheCaesar',
+      'ShortHairTheCaesarSidePart'
+    ],
+    accessoriesType: [
+      'Blank'
+    ],
+    hairColor: [
+      'Auburn',
+      'Black',
+      'Blonde',
+      'BlondeGolden',
+      'Brown',
+      'BrownDark',
+      'PastelPink',
+      'Platinum',
+      'Red',
+      'SilverGray'
+    ],
+    facialHairType: [
+      'Blank',
+      'BeardMedium',
+      'BeardLight',
+      'BeardMagestic',
+      'MoustacheFancy',
+      'MoustacheMagnum'
+    ],
+    clotheType: [
+      'BlazerShirt',
+      'BlazerSweater',
+      'CollarSweater',
+      'GraphicShirt',
+      'Hoodie',
+      'Overall',
+      'ShirtCrewNeck',
+      'ShirtScoopNeck',
+      'ShirtVNeck'
+    ],
+    eyeType: [
+      'Default',
+      'EyeRoll',
+      'Happy',
+      'Side',
+      'Wink',
+      'WinkWacky'
+    ],
+    eyebrowType: [
+      'Default',
+      'DefaultNatural',
+      'FlatNatural',
+      'RaisedExcited',
+      'RaisedExcitedNatural',
+      'UnibrowNatural',
+      'UpDown',
+      'UpDownNatural'
+    ],
+    mouthType: [
+      'Concerned',
+      'Default',
+      'Disbelief',
+      'Serious',
+      'Smile',
+      'Twinkle'
+    ],
+    skinColor: [
+      'Tanned',
+      'Yellow',
+      'Pale',
+      'Light',
+      'Brown',
+      'DarkBrown',
+      'Black'
+    ]
+  };
